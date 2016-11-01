5bcde24af6efb4cb38bd538e2c1e21200e24a005
pyzhou-talend <pyzhou@talend.com>
2016-11-01 09:43:01 +0800

    test conflict

--- Demo/src/first/First.java
+++ Demo/src/first/First.java
@@ -1,19 +1,24 @@
 package first;
 
 public class First {
+	class Dog {
+		public Dog(){
+			
+		}
+		public void print (){
+			System.out.println("dudulu");
+		}
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is the first line");
 		for(int i = 0 ; i<5 ; i++){
 			System.out.println("lalala");
+			Dog dog = null;
 			
 		}
-		class Dog {
-			public void print (){
-				System.out.println("dudulu");
-			}
-		}
+		
 
 	}
 
