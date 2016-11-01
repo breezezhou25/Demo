da5da878f770777d3c9e39acf4db53a9ae95fb97
pyzhou-talend <pyzhou@talend.com>
2016-11-01 09:28:21 +0800

    test add

---
+++ Demo/src/first/First.java
@@ -0,0 +1,14 @@
+package first;
+
+public class First {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is the first line");
+		for(int i = 0 ; i<5 ; i++){
+			System.out.println("lalala");
+		}
+
+	}
+
+}
